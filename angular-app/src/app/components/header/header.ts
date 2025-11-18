81b2d6e4025660dceb05a5ac168900287abbf348
Nazar Stepan <64284448+StepanNazar@users.noreply.github.com>
2025-11-18 23:52:52 +0200

    feat: enhance header and footer layout with styling and responsive design

--- angular-app/src/app/components/header/header.ts
+++ angular-app/src/app/components/header/header.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    document.body.style.overflow = '';
+  }
 }
