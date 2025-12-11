6bc7fddda296a155978daf031e79130b37cb2237
Nazar Stepan <64284448+StepanNazar@users.noreply.github.com>
2025-12-12 00:47:07 +0200

    feat: add supabase storage

--- api/prod.py
+++ api/prod.py
@@ -1,7 +1,10 @@
 import os
 
 from api import create_app
-from api.blueprints.uploads.services import LocalFolderStorageService
+from api.blueprints.uploads.services import (
+    LocalFolderStorageService,
+    SupabaseStorageService,
+)
 from api.config import Config
 
 
@@ -10,7 +13,18 @@ class ProdConfig(Config):
     SECRET_KEY = os.environ["SECRET_KEY"]
     JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
     DEBUG = False
-    STORAGE_SERVICE = LocalFolderStorageService()
+    if (
+        os.environ.get("SUPABASE_URL")
+        and os.environ.get("SUPABASE_KEY")
+        and os.environ.get("SUPABASE_BUCKET")
+    ):
+        STORAGE_SERVICE = SupabaseStorageService(
+            os.environ["SUPABASE_URL"],
+            os.environ["SUPABASE_KEY"],
+            os.environ["SUPABASE_BUCKET"],
+        )
+    else:
+        STORAGE_SERVICE = LocalFolderStorageService()
     JWT_COOKIE_CSRF_PROTECT = True
 
 
